4697389f14492e3a59aee8a75d9e4a7a6fe3cedb
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-08-03 17:46:03 -0300

    Add files via upload
    puse los apuntes sobre los methodos de los arrays que todavia me faltan ejercitar. 
    Ademas empecé el generador de personas aleatorias, tengo cerca de 100 apellidos.

--- estructuraDatos/scripts.js
+++ estructuraDatos/scripts.js
@@ -20,6 +20,71 @@ const enlacesInteresantes = [
 ]
 
 
+/*
+creador de personas aleatoreas
+*/
+
+let nombresHombres = ["juan", "carlos", "ernesto", "ezequiel", "damian", "cristian", "sergio", "jonathan", "brian", "nelson", 
+"leandro", "genaro", "claudio", "ignacio", "david", "luciano", "ismael", "eugenio", "francisco", "gervasio", "oscar", "enrique"];
+
+let apellidos = ["Gonzalez", "perez", "rodríguez", "fonseca", "castro", "hernández", "villalba", "luna", "olivera", "mansilla", "ponce", "sosa", "quiroga",
+"ramírez", "juárez", "fernández", "acosta", "campos", "gracía", "navarro", "vargas", "moreno", "álvarez", "blanco", "cruz", "godoy", "molina",
+"soria", "lópez", "torres", "herrera", "giménez", "carrizo", "domínguez", "peralta", "acuña", "benítez", "chávez", "muñoz",
+"paz", "sanchez", "vera", "ruiz", "silva", "aguero", "castillo", "ledesma", "moyano", "pérez, rivero", "soto", "torre", "vega",
+"cáceres", "arias", "bustos", "córdoba", "farías", "leiva", "maldonado", "nuñez", "ojeda", "velázquez", "toledo", "roldán", "pereyra", "miranda", "lucero",
+"ferreyra", "vázquez", "díaz", "bravo", "ávila", "barrios", "cabrera", "domínguez", "figueroa", "gutiérrez", "russo", "maidana", "ortiz", "rojas", "ayala", "correa",
+"medina", "valdez", "rivero", "peralta", "morales", "gímenez", "franco", "martín", "páez", "ramos", "aguirre", "cáceres", "duarte", "flores", "mendoza", "martínez", "ríos", "coronel", "méndez", "romero"]
+
+let proviciasArgentinas = ["buenos aires", "santa fe", "cordoba", "tierra del fuego", "santa cruz", "chubut", "rio negro", "la pampa", "entre rios", "san luis", "san juan",
+ "mendoza", "catamarca", "misiones", "salta", "jujuy", "ciudad autonoma de buenos aires", "santiago del estero", "formosa"];
+
+//altura en Cm. Peso en Kilogramos
+const personaEjemplo = [
+    {   id: 0,
+        nombres: "juan carlos",
+        apellidos: "gonzalez",
+        colorPelo: "castaño",
+        colorOjos: "castaño",
+        edad: 22,
+        altura: 164,
+        peso: 70,    
+        provincia: "Buenos Aires",
+    }
+]
+
+//devuelve un numero aleatorio entre dos valores
+const getNumeroAleatorio = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+} 
+//console.log(getNumeroAleatorio(0, apellidos.length - 1));
+
+const crearPersonaAzar = () => {
+    let primerNombreAzar = nombresHombres[getNumeroAleatorio(0, nombresHombres.length - 1)];
+    let segundoNombreAzar =  nombresHombres[getNumeroAleatorio(0, nombresHombres.length - 1)];
+    let apellidoAzar = apellidos[getNumeroAleatorio(0, apellidos.length - 1)];
+    let colorPeloAzar = "castaño";
+    let colorOjosAzar = "castaño";
+    let edadAzar = 22;
+    let alturaAzar = 164;
+    let pesoAzar = 70;
+    let provinciaAzar = "Buenos Aires";
+
+    return {   id: 0,
+        nombres: primerNombreAzar + " " + segundoNombreAzar,
+        apellidos: apellidoAzar,
+        colorPelo: colorPeloAzar,
+        colorOjos: colorOjosAzar,
+        edad: edadAzar,
+        altura: alturaAzar,
+        peso: pesoAzar,    
+        provincia: provinciaAzar,
+    }    
+}
+
+
+
 /* Accediendo a los elementos del array y estructuras
  ****************************************************/
 console.log(animales);
@@ -54,7 +119,26 @@ const primerasLetrasColores = colores.map(color => leerPrimeraLetra(color));
 console.log(colores);
 console.log(primerasLetrasColores);
 
+
 /* Ejemplos para filter: Usar un array de objetos alumnos,
 O personas o algo que tengan distintas características, y filtrar por
 Esas características, generando un array con los resultados
+
+Map
+Generar nuevos arrays con atributos buscados dentro de un array o array de objetos en particular. 
+Aclarar que devuelve un array de la misma longitud que el original.
+Por ejemplo, generar un nuevo array con el mismo array de objetos, pero con menos atributos en cada uno.
+Devolver un nuevo array con atributos momificados, como por ejemplo, 
+sacar los primeros nombres de un atributo del tipo "nombre y apellido"
+Some
+Devuelve true o false. 
+Revisa si ALGUNO de los elementos cumple con cierta condicion. Al menos uno de los elementos.
+
+Sort
+Sirve para ordenar arrays
+Utiliza la función de comparación.
+Let ArrayOrdenado
 */
+
+console.log(crearPersonaAzar());
+
