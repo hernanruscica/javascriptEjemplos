234af79b51e66bb25ae8464d558adb6dac524f72
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-08-05 15:45:48 -0300

    Add files via upload
    mas ejemplos de funciones de arrays.

--- estructuraDatos/scripts.js
+++ estructuraDatos/scripts.js
@@ -130,7 +130,7 @@ console.log(apellidosEmpiezanConC);
 
 
 
-/*
+/************************************************************************** 
 SOME
 busca dentro de una array, viendo si se cumple una cierta condicion, en al menos alguno de los elementos. 
 Devuelve true o false. 
@@ -148,7 +148,20 @@ let hayAlgunColorEmpieceConLetraB = colores.some(color => hayAlgunColorEmpieceCo
 console.log(`hay algun color que empiece con la letra ${letraBuscada}?: `, hayAlgunColorEmpieceConLetraB)
 
 
-/*
+/*********************************************************************************
+FIND : Busca un elemento, si lo encuentra, devuelve dicho elemento. Devuelve solo la primer ocurrencia.
+*/
+//primer ejemplo de busqueda FIND en un array de strings
+let elementoEncontrado = colores.find(elemento => elemento === "rojo")
+console.log(elementoEncontrado);
+
+//segundo ejemplo de busqueda FIND en un array de objetos, devuelve el objeto que tiene ese id
+elementoEncontrado = enlacesInteresantes.find(enlace => enlace.id === 1);
+console.log(elementoEncontrado);
+
+
+
+/*********************************************************
 SORT: Sirve para ordenar arrays. Utiliza la función de comparación.
 Para cambiar el orden, hay que invertir el orden
 */
@@ -166,3 +179,87 @@ const proviciasOrdenAlfabetico = proviciasArgentinaMayus.sort((a, b) => {
 console.log(proviciasOrdenAlfabetico);
 
 
+/********************************************************************
+ FOREACH: 
+ Permite iterar el contenido de un array. Recibe un callback que toma como parámetro el elemento actual de la iteración y el indice del mismo.  
+ */
+  
+ nombresHombres.forEach(elemento => {
+    //console.log(elemento);   
+ })
+ 
+
+/**********************************************************
+ REDUCE: 
+ Aplica una función a un acumulador y a cada valor de una array (de izquierda a derecha) para reducirlo a un único valor.  
+************************************************************/
+
+//ejemplo 1, hago una sumatoria de los precios dentro del array precios
+let costoTotal = precios.reduce((acumulador, precioActual) => acumulador + precioActual);
+console.log(costoTotal);
+
+//ejemplo 2, si quisiera hacer una sumatoria de valores, dentro de un objeto, primero deberia hacer un array de esos valores
+let preciosProductos = productos.map(producto => producto.precio);
+let sumatoriaPreciosProductos =  preciosProductos.reduce((acumulador, precio) => acumulador + precio);
+console.log(sumatoriaPreciosProductos);
+
+/**********************************************************
+ INDEXOF: 
+ Devuelve el numero del indice donde se haya producido la primera ocurrencia de una busqueda dada.
+************************************************************/
+
+//primer ejemplo, busco el orden de una provincia dentro del array de provincias
+let provinciaBuscada = 'Mendoza';
+let indiceProvinciaEncontrada = proviciasOrdenAlfabetico.indexOf(provinciaBuscada);
+console.log(`La provincia de ${provinciaBuscada} está en el orden: ${indiceProvinciaEncontrada}`);
+
+
+/*************************************
+ FILL
+ Cambia todos los elementos de un array por un valor estático, desde el índice de inicio hasta el índice final. 
+ Retorna el array modificado. Si el array está vacio no funciona.
+ **************************************************************************************/
+let arrayLlenoCeros = [0, 1, 2];
+//fill(elementoDeRelleno, posicionInicial (inclusiva), posicionFinal (exclusiva))
+arrayLlenoCeros.fill(0, 1, 3);
+console.log(arrayLlenoCeros);
+
+/**************************************************************
+ PUSH
+ Inserta un nuevo elemento al final de un array y devuelve la nueva longitud del mismo.
+ *****************************************************************/
+ let longitudDespuesPush = arrayLlenoCeros.push(1);
+ console.log(`Ahora el array tiene ${longitudDespuesPush}, y lleno con : ${arrayLlenoCeros}`);
+
+
+ /**************************************************************
+ POP
+ Elimina y devuelve el ultimo elemento del array, modificando la longitud del mismo
+ *****************************************************************/
+let ultimoElementoArray = colores.pop();
+let longitudDespuesPop = colores.length;
+console.log(`Con pop() saqué ${ultimoElementoArray} y los colores ahora son ${colores} con una longitud de ${longitudDespuesPop}`);
+
+
+ /**************************************************************
+ UNSHIFT
+ Agrega uno o más elementos al inicio del array, y devuelve la nueva longitud del array.
+ *****************************************************************/
+
+ //primer ejemplo, agrego un solo elemento
+ let longitudDespuesUnshift = colores.unshift(ultimoElementoArray);
+console.log(`Con unshift() pusé ${ultimoElementoArray} y los colores ahora son \n${colores} con una longitud de ${longitudDespuesUnshift}`);
+
+//ejemplo de agregar dos elementos al comienzo del array
+longitudDespuesUnshift = colores.unshift("salmon", "gris");
+console.log(`Ahora con unshift() puse dos elementos a la vez y los colores ahora son \n${colores} con una longitud de ${longitudDespuesUnshift}`);
+
+
+ /**************************************************************
+ SHIFT
+  Elimina el primer elemento del array y lo retorna. Este método modifica la longitud del array.
+ *****************************************************************/
+
+  let primerElementoArray =  colores.shift();
+  let longitudDespuesShift = colores.length;
+  console.log(`Con shift() saqué ${primerElementoArray} y los colores ahora son ${colores} con una longitud de ${longitudDespuesShift}`);
